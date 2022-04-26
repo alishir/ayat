6efaf2bc068c99a930e982b5820005bb4e248cf4
Ali Shirvani <alishirv@pm.me>
2022-04-26 19:17:23 +0430

    بسم الله الرحمن الرحیم
    الحمدلله رب العالمین وصلی الله علی سیدنا محمد وآله الطاهرین سیما بقیة
    الله فی الارضین.

---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'آیات الولایة',
+  tagline: 'در فضائل امیرالمومنین علی علیه السلام',
+  url: 'https://alishir.github.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'alishir', // Usually your GitHub org/user name.
+  projectName: 'ayat', // Usually your repo name.
+  
+  i18n: {
+	defaultLocale: 'fa',
+	locales: ['fa', 'ar', 'en'],
+	localeConfigs: {
+	  en: {
+		label: 'English',
+		direction: 'ltr',
+		htmlLang: 'en-US',
+	  },
+	  fa: {
+		label: 'فارسی',
+		direction: 'rtl',
+		htmlLang: 'fa-IR',
+	  },
+	},
+  },
+  
+  presets: [
+	[
+	  'classic',
+	  /** @type {import('@docusaurus/preset-classic').Options} */
+	  ({
+		docs: {
+		  sidebarPath: require.resolve('./sidebars.js'),
+		  // Please change this to your repo.
+		  editUrl: 'https://github.com/alishir/ayat/',
+		},
+		blog: {
+		  showReadingTime: true,
+		  // Please change this to your repo.
+		  editUrl:
+		  'https://github.com/alishir/ayat/',
+		},
+		theme: {
+		  customCss: require.resolve('./src/css/custom.css'),
+		},
+	  }),
+	],
+  ],
+
+  themeConfig:
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  ({
+	navbar: {
+	  title: 'آیات الولایة',
+	  logo: {
+		alt: 'Amir Tile',
+		src: 'img/amir.png',
+	  },
+	  items: [
+		{
+		  type: 'doc',
+		  docId: '01/01',
+		  position: 'left',
+		  label: 'سوره‌ها',
+		},
+/**		{type: 'localeDropdown', position: 'right',}, */
+/**		{to: '/blog', label: 'Blog', position: 'right'}, */
+		{
+		  href: 'https://github.com/alishir/ayat',
+		  label: 'GitHub',
+		  position: 'right',
+		},
+	  ],
+	},
+	footer: {
+	  style: 'dark',
+	  links: [ ],
+/**	  copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`, */
+	},
+	prism: {
+	  theme: lightCodeTheme,
+	  darkTheme: darkCodeTheme,
+	},
+  }),
+};
+
+module.exports = config;
