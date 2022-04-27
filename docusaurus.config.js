e95a302cf761270adf731c49b0fd87d78b704620
Ali Shirvani <alishirv@pm.me>
2022-04-27 23:31:08 +0430

    update editUrl.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,13 +41,13 @@ const config = {
 		docs: {
 		  sidebarPath: require.resolve('./sidebars.js'),
 		  // Please change this to your repo.
-		  editUrl: 'https://github.com/alishir/ayat/',
+		  editUrl: 'https://github.com/alishir/ayat/edit/main/',
 		},
 		blog: {
 		  showReadingTime: true,
 		  // Please change this to your repo.
 		  editUrl:
-		  'https://github.com/alishir/ayat/',
+		  'https://github.com/alishir/ayat/edit/main/',
 		},
 		theme: {
 		  customCss: require.resolve('./src/css/custom.css'),
