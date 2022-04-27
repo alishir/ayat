f449223615612d2d77bccb8981464eb5452e25f2
Ali Shirvani <alishirv@pm.me>
2022-04-27 23:18:29 +0430

    Deploy website - based on beafd1c3ac800f8656653d5209e29861f6231270

---
+++ assets/js/f147378d.f308fa7d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkayat=self.webpackChunkayat||[]).push([[629],{5745:function(a){a.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-pages","id":"default"}}')}}]);
